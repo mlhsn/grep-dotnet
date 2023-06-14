e83ceff46553e61607b599490b29b75aed67536c
Hasan BD.DHK <mahmudul.hasan@monstar-lab.com>
2023-06-14 13:06:16 +0600

    feat: add help contexts to values

--- Application/Console/Options.cs
+++ Application/Console/Options.cs
@@ -4,10 +4,10 @@ namespace Application.Console
 {
     class Options
     {
-        [Value(0)]
+        [Value(0, HelpText = "Pattern string to search", MetaName = "PATTERN", Required = true)]
         public string Pattern { get; set; } = string.Empty;
 
-        [Value(1)]
+        [Value(1, HelpText = "File[s] to search for pattern", MetaName = "FILE[S]...", Required = true, Min = 1)]
         public IEnumerable<string> InputFiles { get; set; } = new List<string>();
     }
 }

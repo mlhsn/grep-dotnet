312f6f8601c3414fc5b75e37832c2c4b80b9a1b5
Hasan BD.DHK <mahmudul.hasan@monstar-lab.com>
2023-09-21 16:31:11 +0600

    feat: search in files

---
+++ src/Domain/Services/Search.cs
@@ -0,0 +1,31 @@
+﻿using Domain.DTO;
+
+namespace Domain.Services;
+
+public class Search
+{
+    public static void ForPattern(in Request request, out List<Result> results)
+    {
+        results = new List<Result> { };
+
+        foreach (var fileName in request.fileNameList)
+        {
+            if (!File.Exists(fileName))
+            {
+                throw new FileNotFoundException($"file `{fileName}` not found.");
+            }
+
+            using (StreamReader sr = File.OpenText(fileName))
+            {
+                String? line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    if (line.Contains(request.pattern))
+                    {
+                        results.Add(new Result { container = line });
+                    }
+                }
+            }
+        }
+    }
+}
